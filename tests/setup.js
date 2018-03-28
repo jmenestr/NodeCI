3ad96393f3aeb05f56d5fef014dde8cba733085d
Justin Menestrina <justin.menestrina@logrhythm.com>
2018-03-28 15:25:24 -0600

    add timeut

--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,6 @@
 require('../models/User')
 const mongoose = require('mongoose')
 const keys =  require('../config/keys')
-
+jest.setTimeout(30000)
 mongoose.Promise = global.Promise
 mongoose.connect(keys.mongoURI, { useMongoClient: true })
