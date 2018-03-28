9e1225def56cfadf4fc86402d120f462d9fe367b
Justin Menestrina <justin.menestrina@logrhythm.com>
2018-03-28 15:43:56 -0600

    fix paths

--- index.js
+++ index.js
@@ -28,13 +28,11 @@ require('./routes/authRoutes')(app);
 require('./routes/blogRoutes')(app);
 
 if (['production', 'ci'].includes(process.env.NODE_ENV)) {
-  
-
   const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'));
-  });
+  app.use('/static', express.static(path.join(__dirname, 'client/build/static')))
+  app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'client/build/static/index.html')))
 }
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
